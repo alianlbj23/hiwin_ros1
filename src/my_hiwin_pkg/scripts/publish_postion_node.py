2ff5389dc2dc56a7aa2ab3262b234f4276385c1f
alianlbj23 <alianlbj23@gmail.com>
2025-06-23 16:07:47 +0800

    Just publish one time

--- src/my_hiwin_pkg/scripts/publish_postion_node.py
+++ src/my_hiwin_pkg/scripts/publish_postion_node.py
@@ -6,22 +6,21 @@ from custom_msgs.msg import Point, PointArray
 if __name__ == "__main__":
     rospy.init_node('my_pointarray_publisher')
     pub = rospy.Publisher('/target_points', PointArray, queue_size=10)
-    rate = rospy.Rate(1)  # 1 Hz
+    rospy.sleep(1.0)  # 等待 publisher 註冊完成
 
-    while not rospy.is_shutdown():
-        msg = PointArray()
-        # 範例：送出三個隨機座標
-        points = [
-            (-0.5, 0.1, -0.8),
-            (-0.6, 0.05, -0.9),
-            (-0.7, 0.02, -0.7)
-        ]
-        for x, y, z in points:
-            pt = Point()
-            pt.x = x
-            pt.y = y
-            pt.z = z
-            msg.points.append(pt)
-        pub.publish(msg)
-        rospy.loginfo("已送出 %d 個點", len(msg.points))
-        rate.sleep()
+    msg = PointArray()
+    # 範例：送出三個座標
+    points = [
+        (-0.4992596209049225, 0.8769174218177795, 0.7969591021537781),
+        (-0.4992596209049225, 0.8769174218177795, 0.7969591021537781),
+        (-0.07023268938064575, 0.8089713454246521, 0.9886242151260376)
+    ]
+    for x, y, z in points:
+        pt = Point()
+        pt.x = x
+        pt.y = y
+        pt.z = z
+        msg.points.append(pt)
+    pub.publish(msg)
+    rospy.loginfo("已送出 %d 個點", len(msg.points))
+    rospy.sleep(0.5)  # 稍等確保訊息有送出去
