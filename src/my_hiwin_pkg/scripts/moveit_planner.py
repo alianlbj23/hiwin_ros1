8d525eac6b4e5d6d08fd3364ac65796edd7e28c3
alianlbj23 <alianlbj23@gmail.com>
2025-08-02 15:41:59 +0800

    Add multi angle mode

--- src/my_hiwin_pkg/scripts/moveit_planner.py
+++ src/my_hiwin_pkg/scripts/moveit_planner.py
@@ -2,6 +2,7 @@
 import rospy
 import actionlib
 import json
+import time
 
 from std_msgs.msg import String
 from moveit_msgs.msg import MoveGroupAction, MoveGroupGoal, Constraints, JointConstraint
@@ -22,35 +23,43 @@ class MoveGroupCommanderFromComplexJSON:
 
     def callback(self, msg):
         try:
-            # 將字串解碼成 JSON 物件
-            outer_data = json.loads(msg.data)
-            joint_data = outer_data["joints"][0]  # 只取第一組 joints
+            data = json.loads(msg.data)
+            joints_list = data.get("joints", [])
 
-            joint_names = joint_data["name"]
-            joint_positions = joint_data["position"]
+            if not joints_list:
+                rospy.logwarn("收到的 joints 清單為空")
+                return
 
-            if len(joint_names) != len(joint_positions):
-                raise ValueError("joint 名稱數量與 position 數量不符")
+            for idx, joint_data in enumerate(joints_list):
+                joint_names = joint_data["name"]
+                joint_positions = joint_data["position"]
 
-            # 建立 MoveGroupGoal
-            joint_constraints = []
-            for name, pos in zip(joint_names, joint_positions):
-                jc = JointConstraint()
-                jc.joint_name = name
-                jc.position = pos
-                jc.tolerance_above = 0.01
-                jc.tolerance_below = 0.01
-                jc.weight = 1.0
-                joint_constraints.append(jc)
+                if len(joint_names) != len(joint_positions):
+                    raise ValueError("joint 名稱數量與 position 數量不符 (第 {} 組)".format(idx))
 
-            goal = MoveGroupGoal()
-            goal.request.group_name = "manipulator"  # 替換為你的 MoveIt group 名稱
-            goal.request.goal_constraints.append(Constraints(joint_constraints=joint_constraints))
+                # 組建每組 JointConstraint
+                joint_constraints = []
+                for name, pos in zip(joint_names, joint_positions):
+                    jc = JointConstraint()
+                    jc.joint_name = name
+                    jc.position = pos
+                    jc.tolerance_above = 0.01
+                    jc.tolerance_below = 0.01
+                    jc.weight = 1.0
+                    joint_constraints.append(jc)
 
-            rospy.loginfo("發送角度到 /move_group ...")
-            self.client.send_goal(goal)
-            self.client.wait_for_result()
-            rospy.loginfo("機械手臂已完成動作")
+                # 建立並發送目標
+                goal = MoveGroupGoal()
+                goal.request.group_name = "manipulator"  # 根據你的 MoveIt group 修改
+                goal.request.goal_constraints.append(Constraints(joint_constraints=joint_constraints))
+
+                rospy.loginfo("發送第 {} 組目標角度至 /move_group...".format(idx + 1))
+                self.client.send_goal(goal)
+                self.client.wait_for_result()
+                rospy.loginfo("第 {} 組角度執行完成。".format(idx + 1))
+
+                # 可選：執行完等待一段時間再做下一組
+                time.sleep(1.0)
 
         except Exception as e:
             rospy.logerr("JSON 解析或 MoveIt 執行錯誤: {}".format(e))
